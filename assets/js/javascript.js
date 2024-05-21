7d54741cabdcdc3ad7882664d3b6c2e1c0e8650e
willy0483 <willyjensen251@gmail.com>
2024-05-21 10:39:55 +0200

    form

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,100 +1,100 @@
-const myUserNameElement = document.getElementById("username");
-const myPasswordElement = document.getElementById("password");
-const myUserEmailElement = document.getElementById("email");
-
-const submitButton = document.getElementById("submitButton");
-
-let isPasswordValid = false;
-let isUsernameValid = false;
-let isEmailValid = false;
-
-submitButton.disabled = true;
-
-myUserNameElement.addEventListener("keyup", () => {
-  let username = myUserNameElement.value;
-  let usernameRegEx = /^[\w\d.-]{4,12}$/;
-  isUsernameValid = usernameRegEx.test(username);
-
-  if (isUsernameValid) {
-    myUserNameElement.style.border = "2px solid green";
-    console.log("valid username");
-  } else {
-    myUserNameElement.style.border = "2px solid orange";
-    console.log("invalid username");
-    const wrongUsername = document.getElementById("labelUsername");
-    wrongUsername.innerText = "username: need to be 4 to 12 characters long";
-  }
-  validateForm();
-});
-
-myPasswordElement.addEventListener("keyup", (e) => {
-  let password = myPasswordElement.value;
-  let passwordRegEx = /^[a-zA-Z0-9]{8,15}$/;
-
-  isPasswordValid = passwordRegEx.test(password);
-
-  if (isPasswordValid) {
-    myPasswordElement.style.border = "1px solid green";
-    myPasswordElement.style.outline = "none";
-    console.log("valid");
-  } else {
-    myPasswordElement.style.border = "1px solid red";
-    myPasswordElement.style.outline = "none";
-    console.log("invalid");
-    const wrongPassword = document.getElementById("labelPassword");
-    wrongPassword.innerText =
-      "Password: need to be 8 to 15 characters long only letters and numbers";
-  }
-  validateForm();
-});
-
-myUserEmailElement.addEventListener("keyup", (e) => {
-  let email = myUserEmailElement.value;
-  let emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-
-  isEmailValid = emailRegEx.test(email);
-
-  if (isEmailValid) {
-    myUserEmailElement.style.border = "1px solid green";
-    console.log("valid email");
-    submitButton.disabled = false;
-  } else {
-    myUserEmailElement.style.border = "1px solid red";
-    console.log("invalid email");
-    const wrongEmail = document.getElementById("labelEmail");
-    wrongEmail.innerText = "Email: need to have a valid email";
-  }
-  validateForm();
-});
-submitButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log("Form submitted successfully!");
-  let formContainer = document.getElementById("formContainer");
-  formContainer.style.display = "none";
-});
-
-function validateForm() {
-  console.log(isPasswordValid, isUsernameValid, isEmailValid);
-  if (isPasswordValid && isUsernameValid && isEmailValid) {
-    submitButton.disabled = false;
-  } else {
-    submitButton.disabled = true;
-  }
-}
-
 const signUp = document.getElementById("signUp");
 const closeSignUp = document.getElementById("closeSignUp");
+const formContainer = document.getElementById("formContainerSignUp");
 
-// SignUp/IN
-// Show
 signUp.addEventListener("click", (e) => {
   formContainer.style.visibility = "visible";
 });
 
-// Close
 closeSignUp.addEventListener("click", (e) => {
   formContainer.style.visibility = "hidden";
-  myUserNameElement.value = "";
-  myPasswordElement.value = "";
-  myUserEmailElement.value = "";
 });
+
+const myUserNameSignUp = document.getElementById("usernameSignUp");
+const myUserEmailSignUp = document.getElementById("emailSignUp");
+const myPasswordSignUp = document.getElementById("passwordSignUp");
+
+const submitButtonSignUp = document.getElementById("submitButtonSignUp");
+
+let isUsernameValidSignUp = false;
+let isEmailValidSignUp = false;
+let isPasswordValidSignUp = false;
+
+submitButtonSignUp.disabled = true;
+
+myUserNameSignUp.addEventListener("keyup", (e) => {
+  let usernameSignUp = myUserNameSignUp.value;
+  let usernameSignUpRegEx = /^[\w\d.-]{4,12}$/;
+  isUsernameValidSignUp = usernameSignUpRegEx.test(usernameSignUp);
+  const wrongUsernameSignUp = document.getElementById("labelUsernameSignUp");
+
+  if (isUsernameValidSignUp) {
+    myUserNameSignUp.style.border = "2px solid green";
+    wrongUsernameSignUp.innerText = "Username";
+    wrongUsernameSignUp.style.fontSize = "1.3rem";
+    console.log("valid username");
+  } else {
+    myUserNameSignUp.style.border = "2px solid orange";
+    wrongUsernameSignUp.innerText = "Username: needs to be 4 to 12 characters ";
+    wrongUsernameSignUp.style.fontSize = "1rem";
+    console.log("invalid username");
+  }
+  validateSignUpForm();
+});
+
+myUserEmailSignUp.addEventListener("keyup", (e) => {
+  let emailSignUp = myUserEmailSignUp.value;
+  let emailSignUpRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+  isEmailValidSignUp = emailSignUpRegEx.test(emailSignUp);
+  const wrongEmailSignUp = document.getElementById("labelEmailSignUp");
+
+  if (isEmailValidSignUp) {
+    myUserEmailSignUp.style.border = "2px solid green";
+    wrongEmailSignUp.innerText = "Email";
+    wrongEmailSignUp.style.fontSize = "1.3rem";
+    console.log("valid email");
+  } else {
+    myUserEmailSignUp.style.border = "2px solid orange";
+    wrongEmailSignUp.innerText = "Email: needs to be a valid email";
+    wrongEmailSignUp.style.fontSize = "1rem";
+    console.log("invalid email");
+  }
+  validateSignUpForm();
+});
+
+myPasswordSignUp.addEventListener("keyup", (e) => {
+  let passwordSignUp = myPasswordSignUp.value;
+  let passwordSignUpRegEx = /^[a-zA-Z0-9]{8,15}$/;
+
+  isPasswordValidSignUp = passwordSignUpRegEx.test(passwordSignUp);
+  const wrongPasswordSignUp = document.getElementById("labelPasswordSignUp");
+
+  if (isPasswordValidSignUp) {
+    myPasswordSignUp.style.border = "2px solid green";
+    wrongPasswordSignUp.innerText = "Password";
+    wrongPasswordSignUp.style.fontSize = "1.3rem";
+    console.log("valid Password");
+  } else {
+    myPasswordSignUp.style.border = "2px solid orange";
+    wrongPasswordSignUp.innerText = "Password: needs to be 8 to 15 characters";
+    wrongPasswordSignUp.style.fontSize = "1rem";
+    console.log("invalid Password");
+  }
+  validateSignUpForm();
+});
+
+submitButtonSignUp.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("Form submitted successfully!");
+  formContainer.style.visibility = "hidden";
+});
+
+function validateSignUpForm() {
+  console.log(isUsernameValidSignUp, isEmailValidSignUp, isPasswordValidSignUp);
+  if (isUsernameValidSignUp && isEmailValidSignUp && isPasswordValidSignUp) {
+    submitButtonSignUp.disabled = false;
+  } else {
+    submitButtonSignUp.disabled = true;
+  }
+}
